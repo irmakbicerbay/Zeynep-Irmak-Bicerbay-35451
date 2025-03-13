a303d5e803c6419e0e390b04d6d6e42bc9e5a420
irmakbicerbay <zirmak2005@gmail.com>
2025-03-13 10:20:30 +0100

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -1,6 +1,6 @@
 first_name = "irmak"
 last_name = "bicerbay"
 index_number = "35451"
-Nationality = "Turkish"
+nationality = "Turkish"
 starting_date = "13.03.2025"
 courses = "math","OOP","English","marketing",
