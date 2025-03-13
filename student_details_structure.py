a8363e881493f8854a1d4751fcfa2ae88bbda163
irmakbicerbay <zirmak2005@gmail.com>
2025-03-13 10:30:33 +0100

    Create student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,14 @@
+{
+    'first_name': 'irmak',
+    'last_name': 'bicerbay',
+    'index_number': 35451,
+    'nationality': 'Turkish',
+    'starting_date': '13.03.2025',
+    'courses': ["Math", "OOP", "Polish", "İntro digi tech", "Telecommunications", "Marketing"]
+}
+print("First Name:", student1['first_name'])
+print("Last Name:", student1['last_name'])
+print("Index Number:", student1['index_number'])
+print("Nationality:", student1['nationality'])
+print("Starting Date:", student1['starting_date'])
+print("Courses:", ", ".join(student1['courses']))
